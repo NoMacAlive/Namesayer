ea6dd2b6ef81c2a0ba9babeda3a08fff94d0b97e
NoMacAlive <37617906+NoMacAlive@users.noreply.github.com>
2018-10-07 12:51:55 +1300

    Revert "Revert "minorchanges""
    This reverts commit e5ff38ab9dad75cca0b57852aa607aebe5b15f19.

--- src/main/java/namesayer/Main.java
+++ src/main/java/namesayer/Main.java
@@ -19,9 +19,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
 //        Config.loadCoinsCountProperty();
-
         Parent root = FXMLLoader.load(getClass().getResource("/MenuScreen.fxml"));
         primaryStage.setTitle("Name Sayer");
         primaryStage.setScene(new Scene(root, 800, 700));
--- src/main/java/namesayer/recording/Config.java
+++ src/main/java/namesayer/recording/Config.java
@@ -1,35 +1,46 @@
 package namesayer.recording;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import namesayer.MenuScreenController;
+import namesayer.ShopController;
 
 import java.io.*;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 /**
  * Defines constants which are mainly folder names
  */
-public class Config {
+public class Config implements Initializable {
     public static final Path CREATIONS_FOLDER = Paths.get("recordings");
     public static final Path TEMP_RECORDINGS = Paths.get("temp");
     public static final Path SAVED_RECORDINGS = Paths.get("saved");
     public static final String WAV_EXTENSION = ".wav";
     public static final Path RATINGS = Paths.get("ratings.txt");
     public static final Path DATA_BASE  = Paths.get("database");
-    public static Label coinCounter = new Label("Coins Collected: 0");
+    public static JFXButton coinCounter;
     public static Integer counter = 0;
     public static Properties coinsCount= new Properties();
 
+    public static MenuScreenController mnCtrl;
+    public static ShopController shopCtrl;
+    public static Parent menuRoot;
+    public static Parent shopRoot;
 
     public static void IncrementCoinCounter() throws IOException {
         counter++;
         coinCounter.setText("Coins Collected: "+counter);
-//        MenuScreenController.getInstance().updateCoinCount(coinCounter.getText());
+
         coinsCount.setProperty("coin",counter.toString());
         coinsCount.store(new FileOutputStream(CREATIONS_FOLDER.resolve("CoinsCounter.txt").toAbsolutePath().toString()),"This is the property of the coins count"+new Date().toString());
     }
@@ -60,15 +71,34 @@ public class Config {
                 InputStream input = new FileInputStream(CREATIONS_FOLDER.resolve("CoinsCounter.txt").toAbsolutePath().toString());
                 coinsCount.load(input);
                 counter = Integer.parseInt(coinsCount.getProperty("coin"));
-                counter--;
-                IncrementCoinCounter();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public Integer getCoinCounter(){
+    public static Integer getCoinCounter(){
         return counter;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        FXMLLoader mnloader = new FXMLLoader(getClass().getResource("MenuScreen.fxml"));
+        FXMLLoader shoploader = new FXMLLoader(getClass().getResource("Shop.fxml"));
+        try {
+            menuRoot = mnloader.load();
+            mnCtrl = mnloader.getController();
+            shopRoot = shoploader.load();
+            shopCtrl = shoploader.getController();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            loadCoinsCountProperty();
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+        }
     }
 }
